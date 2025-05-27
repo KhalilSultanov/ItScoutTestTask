62d60c39f6b8fbf88f5265145a8f4d5a5fd20a34
Khalil <h.sultanoff@yandex.ru>
2025-05-27 17:53:33 +0300

    update README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 ./wait-for-it.sh postgres:5432 --timeout=30
 
-echo "🚀 Running migrations..."
+echo "Running migrations..."
 python manage.py migrate
 
-echo "🔍 Running tests..."
+echo "Running tests..."
 pytest -v
 
-echo "🌐 Starting server..."
+echo "Starting server..."
 python manage.py runserver 0.0.0.0:8000
